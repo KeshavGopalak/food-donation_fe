56218ad5a64bf30a0b6ca50b16ed8c5449157471
Interst3llarYT <krish.gopalk736@gmail.com>
2026-06-01 22:35:57 -0400

    Fixed more bugs & added more styling

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
-  /* config options here */
+   images: {
+    remotePatterns: [
+      {
+        hostname: 'image.kkday.com'
+      }
+    ]
+  }
 };
 
 export default nextConfig;
