4a435b6d167dc2bf3b55299f31b4e8cd3eb627cd
Interst3llarYT <krish.gopalk736@gmail.com>
2026-06-10 22:26:10 -0400

    Continued Hero section

--- Home/HeroSection.tsx
+++ Home/HeroSection.tsx
@@ -1,14 +1,17 @@
 import { BadgeCheck, Truck } from "lucide-react";
-import { BiGame } from "react-icons/bi";
 import BgImage from "../public/4d25b8ac71caeaac3ebc624c453ba1c8ed833c5d.png";
 import Image from "next/image";
-
+import BgImage2 from "../public/37fb07bccbf315b4318b1d18780f8b2a5cfe7e97.png";
 
 export default function HeroSection() {
   return (
     <div className="main w-full h-screen  gap-10 font-inter">
       <div className="w-full h-screen flex flex-col items-center justify-center gap-3 ml-5 font-inter bg-linear-to-r from-[#F9FAFB] via-[#F9FAFBE5] to-[#F9FAFB00]">
-        <Image src={BgImage} alt="Background" className="absolute top-0 left-0 w-full h-full object-cover opacity-15" />
+        <Image
+          src={BgImage}
+          alt="Background"
+          className="absolute top-0 left-0 w-full h-full object-cover opacity-3"
+        />
         <div className="label bg-[#10B98133] text-darkgreen px-2 py-1 rounded-lg mr-auto flex items-center justify-center gap-1">
           <BadgeCheck className="inline-block w-6 mr-2" />
           <span className="text-xs font-bold uppercase flex flex-col items-center">
@@ -38,7 +41,7 @@ export default function HeroSection() {
         </div>
       </div>
 
-      <div className="info">
+      <div className="info w-full flex flex-col items-center justify-center gap-5">
         <div className="boxes flex flex-row gap-4 items-center justify-center">
           <div className="green w-200 h-50 bg-[#10B981] rounded-4xl flex flex-col pl-10 justify-center gap-10">
             <div className="g1 flex flex-col items-start gap-1">
@@ -46,7 +49,9 @@ export default function HeroSection() {
               <span>Meals Saved & Redistributed</span>
             </div>
 
-            <span className="text-xs mt-5 ml-10">Join 5,000+ active change-makers</span>
+            <span className="text-xs mt-5 ml-10">
+              Join 5,000+ active change-makers
+            </span>
           </div>
 
           <div className="orange w-100 h-50 bg-[#F59E0B] rounded-4xl flex flex-col items-center justify-center">
@@ -55,6 +60,29 @@ export default function HeroSection() {
             <span>Deliveries Today</span>
           </div>
         </div>
+        
+          <div className="white w-305 h-50 bg-[#EFF4FF] rounded-4xl flex flex-row items-start justify-around gap-2 border border-[#BBCABF]">
+          <div className="flex flex-col gap-5 pl-5 mt-auto mb-auto mr-5">
+            <span className="rounded-full flex items-center justify-center mr-auto  text-2xl font-bold">
+              Our Local Footprint
+            </span>
+            <span className="text-sm font-medium w-130 text-gray-600">
+              We operate across 45 major urban centers, ensuring no edible food
+              goes to waste while people are hungry.
+            </span>
+          </div>
+          <div className="whiteimg w-150 h-40 rounded-2xl mr-5 overflow-hidden flex items-center justify-center flex-row mt-auto mb-auto">
+          <Image
+            src={BgImage2}
+            alt="Background"
+            className="w-full h-full object-cover"
+          />
+        </div>
+        </div>
+        
+
+        
+        
       </div>
     </div>
   );
